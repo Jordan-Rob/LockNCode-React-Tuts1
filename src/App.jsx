035cfdf16bf0c20336972babfd735c9466c1b407
jordanrob709 <jordanrob709@gmail.com>
2020-05-05 14:55:37 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The end for today</h1>
+      </div>
+    );
+  }
+}
