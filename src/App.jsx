aaacea27d31b2ea2aa3e39d4132509b40a732799
jordanrob709 <jordanrob709@gmail.com>
2020-05-06 17:37:18 +0300

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,14 @@ class App extends Component {
 export default App;*/
 
 import React, { Component } from "react";
+import Footer from "./Footer";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Just startin out with React</h1>
+        <Footer />
       </div>
     );
   }
