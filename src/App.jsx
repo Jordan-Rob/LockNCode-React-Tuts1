c17f263897119f9c2dd9b4bf69481320d8782428
jordanrob709 <jordanrob709@gmail.com>
2020-05-06 17:21:35 +0300

    day2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+/*import React, { Component } from "react";
 
 class App extends Component {
   render() {
@@ -9,4 +9,18 @@ class App extends Component {
     );
   }
 }
+export default App;*/
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Just startin out with React</h1>
+      </div>
+    );
+  }
+}
+
 export default App;
