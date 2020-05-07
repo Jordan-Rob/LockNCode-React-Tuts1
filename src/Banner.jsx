ab2300e886ef8585296e56bfac9d7935e8c66b4e
jordanrob709 <jordanrob709@gmail.com>
2020-05-07 23:14:03 +0300

    day3 use of css in react components

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import "./index.css";
 
 class Banner extends Component {
   render() {
+    const myStyle = {
+      color: "green",
+      backgroundColor: "black",
+      sontSize: 22,
+    };
+
     return (
       <div>
-        <h2>This is Banner</h2>
+        <h2 className="first-class">This is Banner</h2>
+        <p style={myStyle}>This is a paragraph</p>
       </div>
     );
   }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "./index.css";
 
 class Footer extends Component {
   render() {
     return (
       <div>
-        <h1>This is Footer</h1>
+        <h1 className="footer-class">This is Footer</h1>
       </div>
     );
   }
